e3288bbce828152fe3d51632c6aa5beeb0460c90
hust <hustdev@hotmail.com>
2021-03-20 13:05:06 +0100

    should fix alterning song title in header bar

--- song.js
+++ song.js
@@ -9,14 +9,29 @@ class Song {
     constructor() {
         this.song_ = null;
         this.song_start_ = null;
+
+        this.song_list_ = [];
     }
 
     setSong(song) {
+        // Check that song will not be reported again
+        if (this.song_list_.includes(song)) {
+            return;
+        }
+
+        // Add song to buffer of last played list
+        this.song_list_.push(song);
+        if (this.song_list_.length > 25) {
+            this.song_list_.splice(0, 1);
+        }
+
+        // Set current song title
         this.song_ = song;
         this.song_start_ = Date.now() / 1000;
     }
 
     getSong() {
+        // Don't output song if title wasn't set in last 15 minutes
         const now = Date.now() / 1000;
         if ((now - this.song_start_) > 15*60) {
             this.song_ = null;
